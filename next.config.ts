eed4dcfe576e0f1914f1236a5cff4d7fa35a9c09
BoozedBunny <info@boozedbunny.com>
2026-05-09 00:36:00 +0000

    update

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // with reactStrictMode the first entry for the arena fails in blackscreen
   reactStrictMode: false,
-  // Falls du später Bilder von externen Domains (wie Strapi) lädst,
-  // kannst du hier auch direkt die remotePatterns vorbereiten:
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '1337',
-        pathname: '/uploads/**',
+        protocol: "https",
+        hostname: "www.boozedbunnytown.com",
+        port: "",
+        pathname: "/media/**",
       },
     ],
   },
